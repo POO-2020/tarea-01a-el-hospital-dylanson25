7de97f9b58c7f84c9e4c1553f09dc8b3015c245f
dylanson25 <dvillarreal0@ucol.mx>
2020-02-21 16:29:09 -0600

    paciente avance

---
+++ Paciente.js
@@ -0,0 +1,17 @@
+import Nombre from "./Nombre.js"
+import Tiempo from "./Tiempo.js"
+import Fecha from "./Fecha.js"
+
+class Paciente{
+    constructor(nombre, fecha, numero){
+        this.fecha = fecha
+        this.nombre = nombre
+        this.numero = numero
+    }
+    getPerfil(){
+        return`${this.nombre.getNombreCompleto()}, ${this.numero}, ${this.fecha.getFecha3()}`
+
+    }
+}
+let perfil = new Paciente(new Nombre("karla", "Sierra", "Rodriguez"), new Fecha(25, 11, 2000), "312-131-9198")
+console.log(perfil.getPerfil())
