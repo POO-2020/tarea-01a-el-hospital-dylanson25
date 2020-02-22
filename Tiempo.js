1731d31f31f79287b1e60df98bcdf85b7d4f4a84
dylanson25 <dvillarreal0@ucol.mx>
2020-02-22 13:17:06 -0600

    correccion

--- Tiempo.js
+++ Tiempo.js
@@ -1,29 +1,19 @@
-
-class Tiempo{
-    constructor(){
-        this.hora1 = new Date()
-        this.hora = this.hora1.getHours()
-        this.minutos = this.hora1.getMinutes()
-        this.periodo
-        
-        if(this.hora < 12) this.periodo = "AM" 
-        else if(this.hora >= 12) this.periodo = "PM"
-        if (this.hora <= 9) this.hora = "0" + this.hora
-        if (this.minutos <= 9) this.minutos = "0" + this.minutos
-        
+export default class Tiempo{
+    constructor(hora, minuto, periodo){
+        this.hora = hora
+        this.minutos = minuto
+        this.periodo = periodo
     }
     getFormato12(){
-        if(this.hora > 12){
-            this.hora = this.hora1.getHours() - 12
-            if (this.hora <= 9) this.hora = "0" + this.hora
+        if(this.periodo === "AM" && this.hora < 12){
+            return `${this.hora}:${this.minutos} ${this.periodo}`
+        }else if(this.periodo === "PM" || this.hora >= 12){
+            this.hora = this.hora - 12
+            return `${this.hora}:${this.minutos} ${this.periodo}`
         }
-       return `${this.hora}:${this.minutos} ${this.periodo}`
+      
     }
     getFormato24(){
         return `${this.hora}:${this.minutos} ${this.periodo}`
     }
 }
-  let hora1 = new Tiempo()
-  let hora2 = new Tiempo()
-  console.log(hora1.getFormato12())
-  console.log(hora2.getFormato24())
